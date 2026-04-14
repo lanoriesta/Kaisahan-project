00175768f0234569b5ac20e88029746d37648df6
Lawrence Angelo Noriesta <40698971+lanoriesta@users.noreply.github.com>
2026-04-14 21:08:06 +0800

    fix img url

--- src/resourcesData.js
+++ src/resourcesData.js
@@ -1,3 +1,7 @@
+import img1 from "./assets/images/images-two/resources-image-one.webp";
+import img2 from "./assets/images/images-two/resources-image-two.webp";
+import img3 from "./assets/images/images-two/resources-image-three.webp";
+
 const resourcesData = [
   {
     id: 1,
@@ -6,7 +10,7 @@ const resourcesData = [
     desc: "President Duterte's statement on the country's incapacity to produce rice self-sufficiently alarmed our organization, KAISAHAN, and raised a number of questions from our partner farmer communities...",
     btnUrl:
       "https://kaisahan.com.ph/posts/statement-on-the-presidents-unwarranted-claim-on-the-philippines-being-not-rice-self-sufficient",
-    imgUrl: "src/assets/images/images-two/resources-image-one.webp",
+    imgUrl: img1,
   },
   {
     id: 2,
@@ -14,7 +18,7 @@ const resourcesData = [
     desc: "President Duterte’s most recent statements on his Boracay land reform plans alarmed KAISAHAN and agrarian reform advocates. In a gathering last June 13, as CNN Philippines (2018) reported...",
     btnUrl:
       "https://kaisahan.com.ph/posts/dutertes-land-reform-misinformed-reform",
-    imgUrl: "src/assets/images/images-two/resources-image-two.webp",
+    imgUrl: img2,
   },
   {
     id: 3,
@@ -23,7 +27,7 @@ const resourcesData = [
     desc: "This manual aims to provide updated and useful information to community-based paralegals, landless farmers, agrarian reform beneficiaries, and advocates on the latest and important agrarian...",
     btnUrl:
       "https://kaisahan.com.ph/posts/updates-to-agrarian-reform-laws-rules-and-regulations-a-paralegal-manual",
-    imgUrl: "src/assets/images/images-two/resources-image-three.webp",
+    imgUrl: img3,
   },
 ];
 
